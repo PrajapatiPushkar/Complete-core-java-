8debc7e93f9e5ea23c20d62b240b57205a3e9949
Prajapatipushkar <kppusakar2018@gmail.com>
2026-01-14 07:20:37 +0530

    added operators and loops

---
+++ src/basics/operators/Logical.java
@@ -0,0 +1,26 @@
+package src.basics.operators;
+
+import java.util.Scanner;
+
+//types: &&(and), ||(or), !(not)
+
+public class Logical {
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+        System.out.println("welcome to ticket discount calculator");
+        System.out.println("please enter your age: ");
+        int age = input.nextInt();
+        System.out.println("Are you are a female? (true/false) ");
+        boolean isFemale = input.nextBoolean();
+
+        if (age < 5){
+            System.out.println("You got 75% discount");
+        } else if (isFemale) {
+            System.out.println("You got 50% discount");
+        } else if (age > 60 && !isFemale) {
+            System.out.println("You got 25% discount");
+        } else {
+            System.out.println("you got no discount");
+        }
+    }
+}
