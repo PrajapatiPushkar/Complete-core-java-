433bbe9c5a9dd89e29492f453e76d756b6f27b4b
Prajapatipushkar <kppusakar2018@gmail.com>
2026-01-19 20:28:16 +0530

    added oops and Switch.java and Ternary.java

---
+++ src/basics/loops/BreakAndContinue.java
@@ -0,0 +1,13 @@
+package src.basics.loops;
+
+public class BreakAndContinue {
+    public static void main(String[] args) {
+        for (int i = 1; i < 200; i++) {
+            if (i == 101){
+                //break;
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
