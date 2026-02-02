54f533f6af326357cee6beb4678558a7ae4f86e0
Prajapatipushkar <kppusakar2018@gmail.com>
2026-02-02 18:42:09 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ src/basics/ExceptionHandingByFaisalMemon/LibraryDemo.java
@@ -0,0 +1,26 @@
+package src.basics.ExceptionHandingByFaisalMemon;
+
+class Library{
+    int availableBooks = 3;
+
+    public void borrowBook(int bookRequested) {
+        try {
+            int[] books = {101, 102, 103};
+            System.out.println("Book Requested : " + books[bookRequested]);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Exception occur, you requested a book that not exits ");
+        } catch (NullPointerException e){
+            System.out.println("Null pointer exception");
+        }finally {
+            System.out.println("Finally block");
+        }
+    }
+}
+
+public class LibraryDemo {
+    public static void main(String[] args) {
+        Library library = new Library();
+        library.borrowBook(5);
+
+    }
+}
