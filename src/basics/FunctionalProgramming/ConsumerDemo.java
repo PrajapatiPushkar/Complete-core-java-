7ad000d92b4394f7633f9230d00a958ffb8f088b
Prajapatipushkar <kppusakar2018@gmail.com>
2026-02-03 18:20:18 +0530

    added FunctionalInterfaces.java and FunctionInterface.java and MethodReferences.java

---
+++ src/basics/FunctionalProgramming/ConsumerDemo.java
@@ -0,0 +1,23 @@
+package src.basics.FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerDemo {
+    public static void main(String[] args) {
+//Consumer<String> consumer1 = (str) -> System.out.println(str); //normally
+        Consumer<String> consumer1 = System.out::println;//method references
+        Consumer<String> consumer2 = str -> System.out.println("Length:" + str.length());
+        consumer1.accept("Hello");
+        consumer1.accept("Hi");
+
+
+        consumer2.accept("Hello");
+        consumer2.accept("Hi");
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Consumer<Integer> multiplyByTwo = n -> System.out.println(n * 2);
+        numbers.forEach(multiplyByTwo);
+    }
+}
