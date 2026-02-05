baaf0978cee7cbf755291df9f7cee1588d5c9a67
Prajapatipushkar <kppusakar2018@gmail.com>
2026-02-05 06:04:51 +0530

    added FunctionalInterfaces.java and FunctionInterface.java and MethodReferences.java

---
+++ src/basics/Streams/StreamsDemo.java
@@ -0,0 +1,23 @@
+package src.basics.Streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamsDemo {
+    public static void main(String[] args) {
+        //Streams --> Assembly line / pipelines
+        List<String> items = Arrays.asList("Apple ", "Banana ", "Mango \n");
+        for (String fruit : items)
+            System.out.print(fruit);
+
+        //using Streams
+        Stream<String> stream = items.stream();
+//        stream.forEach((item) -> System.out.println(items));
+        stream.forEach(System.out::print);
+
+        Stream<String> stream1 = items.stream();
+         Stream<String> filteredStream =  stream1.filter(name -> name.startsWith("B"));
+        filteredStream.forEach(System.out::print);
+    }
+}
